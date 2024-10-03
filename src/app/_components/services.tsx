5ec33db44b450be532f6a716d5c38737a99fa912
zibby-zibs <oloyedetimi@gmail.com>
2024-10-03 23:57:17 +0100

    chore: added link sto service cards

--- src/app/_components/services.tsx
+++ src/app/_components/services.tsx
@@ -1,6 +1,7 @@
 import { cleaning, iron_cloth, repair } from "@/assets/images";
 import React from "react";
 import ServiceCard from "./service-card";
+import Link from "next/link";
 
 type Props = {};
 
@@ -11,18 +12,21 @@ const Services = (props: Props) => {
       image: cleaning,
       title:
         "Leave the dusting, scrubbing, and vacuuming to us! Our cleaning pros ensure every corner of your home is sparkling clean.",
+      href: "/services/cleaning",
     },
     {
       id: 2,
       image: iron_cloth,
       title:
         "Say goodbye to laundry day blues! We offer quick, efficient laundry and ironing services tailored to your schedule.",
+      href: "/services/laundry",
     },
     {
       id: 3,
       image: repair,
       title:
         "Got a leaky faucet or an electrical issue? Our certified technicians are on call to fix your home maintenance problems.",
+      href: "/services/repair",
     },
   ];
   return (
@@ -33,11 +37,9 @@ const Services = (props: Props) => {
 
       <div className="mt-[70px] flex flex-wrap justify-center gap-4">
         {theServices.map((service) => (
-          <ServiceCard
-            key={service.id}
-            image={service.image}
-            title={service.title}
-          />
+          <Link href={service.href} key={service.id}>
+            <ServiceCard image={service.image} title={service.title} />
+          </Link>
         ))}
       </div>
     </main>
