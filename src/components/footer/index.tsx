484b276351a02ee913384fe9d1d3cf8cb441b66e
serenity <serenityvimo@gmail.com>
2025-02-17 14:35:58 +0100

    Update index.tsx

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -139,13 +139,14 @@ const Footer = (props: Props) => {
                   </div>
                   <div className="flex gap-2 items-center text-lg">
                     <PhoneCall />
-                    07053964706
+                    <a href="https://wa.me/+2348135518126?text=Hello%2C%20I%27m%20contacting%20you%20from%20your%20website%serenity.ng">0813 551 8126</a>
                   </div>
                   <div className="flex gap-2 items-center text-lg">
                     <Instagram />
-                    serenity_official
+                    <a href="https://www.instagram.com/serenity.ng_official"> serenity.ng_official</a>
+                  
                   </div>
-                </div>
+                </div>   
               </div>
             </div>
           </aside>
